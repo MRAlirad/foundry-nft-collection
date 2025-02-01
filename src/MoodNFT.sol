108e40f7dabca3d1948313fc4be542626f5d1737
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2025-02-01 23:50:15 +0330

    Encoding SVGs to be stored onchain

--- src/MoodNFT.sol
+++ src/MoodNFT.sol
@@ -7,19 +7,53 @@ import { Base64 } from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNFT is ERC721{
     uint256 private s_tokenCounter;
-    string private s_sadSvg;
-    string private s_happySvg;
+    string private s_sadSvgImageUri;
+    string private s_happySvgImageUri;
 
-    constructor(string memory sadSvg, string memory happySvg) ERC721("Mood NFT", "MN"){
+    enum Mood {
+        Happy,
+        Sad
+    }
+    
+    mapping (uint256 => Mood) private s_tokenIdToMood;
+
+    constructor(string memory sadSvgImageUri, string memory happySvgImageUri) ERC721("Mood NFT", "MN"){
         s_tokenCounter = 0;
-        s_sadSvg = sadSvg;
-        s_happySvg = happySvg;
+        s_sadSvgImageUri = sadSvgImageUri;
+        s_happySvgImageUri = happySvgImageUri;
     }
 
     function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
+        s_tokenIdToMood[s_tokenCounter] = Mood.Happy;
         s_tokenCounter++;
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {}
+    function _baseURI() internal pure override returns(string memory){
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        string memory imageURI;
+
+        if(s_tokenIdToMood[tokenId] == Mood.Happy) imageURI = s_happySvgImageUri;
+        else imageURI = s_sadSvgImageUri;
+
+        return string(
+            abi.encodePacked(
+                _baseURI(),
+                Base64.encode(
+                    bytes(
+                        abi.encodePacked(
+                            '{"name: "',
+                            name(),
+                            '", description: "An NFT that reflects your mood!", "attributes": [{"trait_type": "Mood", "value": 100}], "image": ',
+                            imageURI,
+                            '"}'
+                        )
+                    )
+                )
+            )
+        );
+    }
 }
